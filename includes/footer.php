bbe3707526f0705c5ab1a61dfbe276c2d93c6e09
Mohammed Bhadsorawala <mhdbhadsorawala@gmail.com>
2024-11-24 21:26:30 +0530

    Adding website code

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   Developed by Batch A group 2 
+                </div>
+
+            </div>
+        </div>
+    </footer>
